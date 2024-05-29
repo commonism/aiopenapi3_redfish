92e1f88244a263c2c4aabd3245bcfd2639958f65
Markus Kötter <koetter@cispa.de>
2024-05-29 12:15:37 +0200

    ci - tests using the DMTF Redfish-Mockup-Server container and DSP2043
    credits for mounting a volume in a github ci service
    https://github.com/orgs/community/discussions/42127#discussioncomment-7591609

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,11 @@ import aiopenapi3_redfish
 @pytest.fixture(scope="session")
 def description_documents():
     return Path(aiopenapi3_redfish.__file__).parent / "description_documents"
+
+
+@pytest.fixture
+def log(caplog):
+    import logging
+
+    caplog.set_level(logging.INFO, logger="httpcore")
+    caplog.set_level(logging.INFO, logger="httpx")
